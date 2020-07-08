60dbf3fbda269eb7f01ca34e09256f0f5f24b69b
qinbishi <qinbishi@xiaomi.com>
2020-07-08 22:21:25 +0800

    重构抓取前引导逻辑

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,38 @@
 var content_div = document.getElementById('content_div');
 console.log(content_div.innerText);
 
-chrome.tabs.query(
-    {
-        'active': true, 
-        'windowId': chrome.windows.WINDOW_ID_CURRENT
-    },
-    function(tabs) {
-        var url = tabs[0].url;
-        console.log(url);
+function get_cur_url(callback) {
+    chrome.tabs.query(
+        {
+            'active': true, 
+            'windowId': chrome.windows.WINDOW_ID_CURRENT
+        },
+        function(tabs) {
+            var url = tabs[0].url;
+            callback(url)
+        }
+    );    
+}
+
+function redirect_url(url) {
+    content_div.innerHTML += `<br>\n<a href=${url} onclick="window.open('${url}')">Click Here</a>`
+}
+
+function detect_env(url) {
+    console.log(url);
+    if (is_article_url(url)) {
         if (is_opened_by_desktop(url)) {
             content_div.innerHTML = "点击后将进行页面切换，然后请耐心等待......";
-            var redirect_url = get_profile_url(url)
+            var to_redirect_url = get_profile_url(url)
             // Todo: 更好的打开新窗口的方法
-            content_div.innerHTML += `<br>\n<a href=${redirect_url} onclick="window.open('${redirect_url}')">Click Here</a>`
+            redirect_url(to_redirect_url)
             // chrome.runtime.sendMessage({redirect: redirect_url})
         } else {
             content_div.innerText = "请先设置微信桌面程序为“使用系统默认浏览器打开网页”，然后使用微信桌面程序打开链接，方可生效。";
-        }
+        }    
+    } else if (is_profile_url(url)) {
+        content_div.innerHTML = "即将开始采集，请稍候......";
     }
-);
+}
+
+get_cur_url(detect_env);
--- js/redirect.js
+++ js/redirect.js
@@ -14,6 +14,26 @@ function getParsedQuery(url) {
     return query;
 }
 
+function is_article_url(url) {
+    // https://mp.weixin.qq.com/s/k0VzLgZbHAqfLbxQj7Ma6Q
+    article_url_pattern1 = "https://mp.weixin.qq.com/s/"
+    // https://mp.weixin.qq.com/s?__biz=MzIxNjM3MDc4Mg==&mid=2247490150&idx=1&sn=b48743cbbd9941b66ae39843a0e765bd&chksm=978b4157a0fcc8416493d44b161ce677e730b713d23ee86af8602ad561ce5ddfd7110f661375&mpshare=1&scene=24&srcid=0701IBiXMBCz0gmb9KohFWWg&sharer_sharetime=1593780737948&sharer_shareid=581c111496045ada57e9602a8e1111af&key=835f76b9f6281b4864d7cda49b9550f52ed87c97a81a0e0e7c21fd3038da653e5d05cdb5513240d56a9ee5ab02751b6f87935b2ee4b30d155b7bc2592052a2ca963805ff4ffa54683687b0e9d434d9c3&ascene=14&uin=MjEwNzc1MTM4MA%3D%3D&devicetype=Windows+10&version=62060833&lang=zh_CN&exportkey=AahS3q6%2BRYUct8oiYwpz7tM%3D&pass_ticket=dzO7jcymWai1yIkubFW5dxZu7s1Gfa%2Fyz%2FAQQDJ6B%2F8JFgWtx2wMTz0j6M%2BdTQWk
+    article_url_pattern2 = "https://mp.weixin.qq.com/s?"
+    if (url.startsWith(article_url_pattern1) || url.startsWith(article_url_pattern2)) {
+        return true;
+    }
+    return false;
+}
+
+function is_profile_url(url) {
+    // https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIxNjM3MDc4Mg==&scene=124&#wechat_redirect
+    profile_url_pattern = "https://mp.weixin.qq.com/mp/profile_ext?"
+    if (url.startsWith(profile_url_pattern)) {
+        return true;
+    }
+    return false;
+}
+
 function is_opened_by_desktop(url) {
     // https://mp.weixin.qq.com/s?__biz=MzI3NDY3NDUxNg==&mid=2247486618&idx=1&sn=3f50e9cb7e8733b51a578cccb3792f02&chksm=eb112aa1dc66a3b7b8fe5a6f8a009cc82fc1c9ccd2c42c25c8302b027eb1fd89096426482554&mpshare=1&scene=24&srcid=07031t4r12ajKUejpUy4Ng6q&sharer_sharetime=1593770983970&sharer_shareid=581c111496045ada57e9602a8e1111af&key=46828b3a71d4b50412e89a4946288b700685f34a5d73e5142e9376338a8de033be6bd6eca666bcab6a8a2cc8858a7856cd50f937191e5aefc694f3f05b44b192c0aad67acdf2737a38e77ab7181b5597&ascene=14&uin=MjEwNzc1MTM4MA%3D%3D&devicetype=Windows+10&version=62060833&lang=zh_CN&exportkey=AYBoSNScuwsQ%2Bdb8Z2t2RqE%3D&pass_ticket=dYq0VXmVoYOnW6y9DcUIoX89rJSCJAxh4QCd7qksLKwHUDv2rdUCIiZ5mhbeZT0q
     var query = getParsedQuery(url)
