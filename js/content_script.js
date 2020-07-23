96d448518a619c40ed4d290232d47a5e0d30c53d
qinbishi <qinbishi@xiaomi.com>
2020-07-24 02:19:24 +0800

    删除冗余代码

--- js/content_script.js
+++ js/content_script.js
@@ -1,10 +1,15 @@
 console.log("Hello From contentScript.js")
 
-// [javascript - Insert code into the page context using a content script](https://stackoverflow.com/questions/9515704/)
-var s = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = chrome.runtime.getURL('js/inject.js');
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+function inject_js(filename) {
+    // [javascript - Insert code into the page context using a content script](https://stackoverflow.com/questions/9515704/)
+    var s = document.createElement('script');
+    // TODO: add "script.js" to web_accessible_resources in manifest.json
+    s.src = chrome.runtime.getURL(filename);
+    s.onload = function() {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
+}
+
+inject_js('js/extract.js')
+inject_js('js/inject.js')
